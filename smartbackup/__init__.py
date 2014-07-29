2f2faeab373e8dccac47f1d9afc3283246dd1463
alex <geniofuturo@gmail.com>
2014-07-29 11:30:05 -0300

    Implementando pacote único(executável) de extensão para o framework 'bakthat'.

--- smartbackup/__init__.py
+++ smartbackup/__init__.py
@@ -1,22 +1,8 @@
-#!/usr/local/bin/python
-import os
-import sys
+import sh
+from bakthat import app
+from helper import BakHelper
 
 __author__ = 'alex'
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # env setup
-sys.path.append(os.path.join(BASE_DIR, "packages"))
-
-import sh
-import bakthat
-
-try:
-    bakthat.EXCLUDE_FILES.remove(".gitignore")
-except ValueError:
-    pass
-
-from bakthat import app
-from backuptools.helper import BakHelper
 
 
 @app.cmd(help="Copies a dump of the database.")
@@ -25,15 +11,9 @@ from backuptools.helper import BakHelper
 @app.cmd_arg('-b', '--backupname', type=str, help='Name (optional) backup file', default='host_mysql')
 @app.cmd_arg('-d', '--destination', type=str, help="s3plus", default='s3plus')
 @app.cmd_arg('-database', '--database', type=str)
-def mysql_dump(user='root', password='', **kwargs):
+def mysqldump(user='root', password='', **kwargs):
     with BakHelper(kwargs['backupname'], destination=kwargs['destination'],
                    password=password, tags=["mysql"]) as bh:
         sh.mysqldump("-p{0}".format(password), u=user, d=kwargs['database'],
                     _out="dump_{0}.sql".format(kwargs['database']))
         bh.backup()
-
-
-if __name__ == '__main__':
-    # run as command line program
-    bakthat.main()
-
---
+++ smartbackup/__main__.py
@@ -0,0 +1,13 @@
+# What is '__main__' module ?
+# Reference - https://docs.python.org/2/using/cmdline.html#cmdoption-m
+
+__author__ = 'alex'
+
+
+def main():
+    import bakthat
+    bakthat.main()
+
+
+if __name__ == '__main__':
+    main()

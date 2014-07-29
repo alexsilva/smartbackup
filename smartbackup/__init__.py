00afb409c72e59a71fb93dbf844fd55452db2e30
alex <geniofuturo@gmail.com>
2014-07-29 16:49:45 -0300

    Adicionando parâmetro host para o dump do mysql.

--- smartbackup/__init__.py
+++ smartbackup/__init__.py
@@ -9,11 +9,20 @@ __author__ = 'alex'
 @app.cmd_arg('-u', '--user', type=str, help='The database administrator.', default='root')
 @app.cmd_arg('-p', '--password', type=str, help='User password', default='')
 @app.cmd_arg('-b', '--backupname', type=str, help='Name (optional) backup file', default='host_mysql')
+@app.cmd_arg('-host', '--host', type=str, help='Mysql host', default=None)
 @app.cmd_arg('-d', '--destination', type=str, help="s3plus", default='s3plus')
 @app.cmd_arg('-database', '--database', type=str)
 def mysqldump(user='root', password='', **kwargs):
     with BakHelper(kwargs['backupname'], destination=kwargs['destination'],
                    password=password, tags=["mysql"]) as bh:
-        sh.mysqldump("-p{0}".format(password), u=user, d=kwargs['database'],
-                    _out="dump_{0}.sql".format(kwargs['database']))
+
+        mysql_kwargs = dict(
+            u=user,
+            d=kwargs['database'],
+            _out="dump_{0}.sql".format(kwargs['database']))
+
+        if kwargs.get('host') is not None:
+            mysql_kwargs['h'] = kwargs['host']
+
+        sh.mysqldump("-p{0}".format(password), **mysql_kwargs)
         bh.backup()

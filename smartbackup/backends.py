34f59a07840b485a841d84cf0929c7b8715cd97b
alex <geniofuturo@gmail.com>
2014-08-04 13:51:28 -0300

    Montando corretamente o nome do arquivo antes de enviar para o s3.

--- smartbackup/backends.py
+++ smartbackup/backends.py
@@ -30,8 +30,8 @@ class S3BackendPlus(backends.S3Backend, BaseBackend):
         return self.bucket.get_key(store_filename) is not None
 
     def upload(self, keyname, filename, **kwargs):
-        keyname = (kwargs['path'] + "/" + keyname) if 'path' in kwargs else keyname
         keyname = server_name_with(self.conf, keyname)
+        keyname = (kwargs['path'] + "/" + keyname) if 'path' in kwargs else keyname
 
         source_size = os.stat(filename).st_size
 
